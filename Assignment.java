6a6100eaa662716f046b7e8ac549cefcd4d3a89f
songeon456 <31638936+songeon456@users.noreply.github.com>
2017-09-06 22:42:10 +0900

    java application 1st assignment

---
+++ Assignment.java
@@ -0,0 +1,13 @@
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2013301033");
+		System.out.println("손건우");
+		System.out.println("01050986961");
+		System.out.println("skygeon93@naver.com");
+
+	}
+
+}
